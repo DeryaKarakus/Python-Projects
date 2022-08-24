22dc551ffc1ac64a1366f0a4b2659f9a72823076
mazgib-gymsu3-waxnaP <derya30900@hotmail.com>
2022-08-24 18:09:41 +0300

    Computer guess the number

--- GuessTheNumber/guess.py
+++ GuessTheNumber/guess.py
@@ -15,4 +15,22 @@ def guess(x):
         
     print(f"Congrats! You have guessed the number {random_number} correctly!")
 
-guess(10)
+
+def computer_guess(x): #computer guesses your number with low or high clues
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high because low = high
+
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (O) ??')
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"Computer guessed your number, {guess}, correctly!")
+
+computer_guess(100)
